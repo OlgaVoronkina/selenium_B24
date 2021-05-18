f148d29ba3d44d7352fbd7c56bd454f5edf2cc1e
OlgaVoronkina <voronkina.olga.s@gmail.com>
2021-05-19 00:36:22 +0300

    1

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.lang.Thread.sleep;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+
+        System.out.println("ввел webdriver ");
+sleep(2000);
+        driver.findElement(By.name("btnK")).click();
+        System.out.println("нажал на поиск");
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver=null;
+    }
+
+}
