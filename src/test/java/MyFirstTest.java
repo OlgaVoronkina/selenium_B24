5ca1ffe4d936ee1de23da7c4f0cec79af6e0d691
OlgaVoronkina <voronkina.olga.s@gmail.com>
2021-05-20 22:20:28 +0300

    2

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -26,6 +26,7 @@ public class MyFirstTest {
         sleep(2000);
         driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
+        System.out.println("");
     }
 
     @After
