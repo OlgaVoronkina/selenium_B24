7c7ba8bf4b8d0582f7839ae43305a41c174ae74c
OlgaVoronkina <voronkina.olga.s@gmail.com>
2021-06-08 17:09:26 +0300

    Задание_11

---
+++ src/test/java/Task_11.java
@@ -0,0 +1,89 @@
+import net.bytebuddy.utility.RandomString;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Random;
+
+import static java.lang.Thread.sleep;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Task_11 extends BaseTest{
+    @Test
+    public void task_11() throws InterruptedException {
+        driver.get("http://localhost/litecart/");
+        wait.until(titleIs("Online Store | My Store"));
+
+//1) регистрация новой учётной записи с достаточно уникальным адресом электронной почты (чтобы не конфликтовало с ранее созданными пользователями, в том числе при предыдущих запусках того же самого сценария),
+        WebElement newCustomers = driver.findElement(By.xpath("//div[@class='content']//a[contains(@href,'create_account')]"));
+        newCustomers.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='title']")));
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='firstname']")).sendKeys("Olga");
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='lastname']")).sendKeys("Voronkina");
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='address1']")).sendKeys("Crimea");
+        String postcode = generatePostcode();
+
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='postcode']")).sendKeys(postcode);
+        String city = generateRandomString(6);
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='city']")).sendKeys(city);
+
+        new Select(driver.findElement(By.xpath("//select[@name='country_code']"))).selectByVisibleText("United States");
+
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='email']")).sendKeys(city+postcode+ "@mail.com");
+        String email = driver.findElement(By.xpath("//div[@class='content']//input[@name='email']")).getAttribute("value");
+
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='phone']")).sendKeys("+123456789");
+
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='password']")).sendKeys("Qwerty123");
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='confirmed_password']")).sendKeys("Qwerty123");
+
+        driver.findElement(By.xpath("//div[@class='content']//button[@name='create_account']")).click();
+
+        if(driver.findElement(By.xpath("//select[@name='zone_code']")).isDisplayed()){
+            sleep(500);
+            new Select(driver.findElement(By.xpath("//select[@name='zone_code']"))).selectByVisibleText("Alaska");
+            driver.findElement(By.xpath("//div[@class='content']//input[@name='password']")).sendKeys("Qwerty123");
+            driver.findElement(By.xpath("//div[@class='content']//input[@name='confirmed_password']")).sendKeys("Qwerty123");
+            driver.findElement(By.xpath("//div[@class='content']//button[@name='create_account']")).click();
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='content']//a[contains(@href,'logout')]")));
+
+//2) выход (logout), потому что после успешной регистрации автоматически происходит вход,
+        driver.findElement(By.xpath("//div[@class='content']//a[contains(@href,'logout')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='content']//button[@name='login']")));
+
+//3) повторный вход в только что созданную учётную запись,
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='email']")).sendKeys(email);
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='password']")).sendKeys("Qwerty123");
+        driver.findElement(By.xpath("//div[@class='content']//button[@name='login']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='content']//a[contains(@href,'logout')]")));
+
+//4) и ещё раз выход.
+        driver.findElement(By.xpath("//div[@class='content']//a[contains(@href,'logout')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='content']//button[@name='login']")));
+
+    }
+
+    public static String generatePostcode() {
+        Random random = new Random();
+        int min = 11111;
+        int max = 99999;
+        int diff = max - min;
+        int i = random.nextInt(diff + 1);
+        i += min;
+        String randomPostcode = String.valueOf(i);
+        return  randomPostcode;
+    }
+
+    public static String generateRandomString(int length) {
+        final String characters = "abcdefghijklmnopqrstuvwxyz";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+            for (int i = 0; i < length; i++) {
+                sb.append(characters.charAt(random.nextInt(characters.length())));
+            }
+        return sb.toString();
+        }
+}
